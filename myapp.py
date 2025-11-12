be857cf0fc4a312b90f51a52cb036084abceb20f
Poresh181 <poresh450@gmail.com>
2025-11-12 18:20:31 +0600

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -2,4 +2,5 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-st.title("K Nearest Neighbors-Build & Deploy(streamlit)")
+st.title("K-Nearest Neighbors-Build & Deploy(Streamlit)")
+st.sidebar.header("Dataset & Preprocessing")
